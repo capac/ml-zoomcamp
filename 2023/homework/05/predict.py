302fab09aa377ada7d6f49fc23ba89dd80aea528
capac <capac@users.noreply.github.com>
2023-10-14 18:39:04 +0100

    Updated with Flask prediction app

--- 2023/homework/05/predict.py
+++ 2023/homework/05/predict.py
@@ -1,15 +1,24 @@
+from flask import Flask, request, jsonify
 import pickle
 
-with open('model1.bin', 'rb') as f_in:
+with open('model1.bin', 'rb') as f_in, open('dv.bin', 'rb') as g_in:
     model = pickle.load(f_in)
-
-with open('dv.bin', 'rb') as g_in:
     dv = pickle.load(g_in)
 
-# Transformation
-client_dict = {"job": "retired", "duration": 445, "poutcome": "success"}
-client = dv.transform(client_dict)
 
-# Prediction
-y_pred = model.predict_proba(client)[:, 1]
-print(f'Probability of getting a credit: {y_pred[0].round(3)}')
+app = Flask('churn')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    client = request.get_json()
+    X = dv.transform([client])
+    y_pred = model.predict_proba(X)[0, 1]
+    results = {
+        'probability': float(y_pred)
+    }
+    return jsonify(results)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
