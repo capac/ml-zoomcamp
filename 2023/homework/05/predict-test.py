517d9b5906c95ef3c241a57c6f6ea21e5a5601db
capac <capac@users.noreply.github.com>
2023-10-15 13:40:03 +0100

    Using model 1 for credit loan outcome

--- 2023/homework/05/predict-test.py
+++ 2023/homework/05/predict-test.py
@@ -3,17 +3,17 @@ import requests
 # parameters
 url = 'http://localhost:9696/predict'
 
-# client for question 2
-# client_1 = {"job": "retired", "duration": 445, "poutcome": "success"}
+# client for question 2: IMPORTANT - REQUIRES model1.bin
+client_1 = {"job": "retired", "duration": 445, "poutcome": "success"}
 
-# client for question 5
-# client_2 = {"job": "unknown", "duration": 270, "poutcome": "success"}
+# client for question 4: IMPORTANT - REQUIRES model1.bin
+client_2 = {"job": "unknown", "duration": 270, "poutcome": "success"}
 
 # client for question 6: IMPORTANT - REQUIRES model2.bin
-client_3 = {"job": "retired", "duration": 445, "poutcome": "success"}
+# client_3 = {"job": "retired", "duration": 445, "poutcome": "success"}
 
-# client_list = [client_1, client_2, client_3]
-client_list = [client_3]
+client_list = [client_1, client_2]
+# client_list = [client_3]
 
 for idx, client in enumerate(client_list):
     result = requests.post(url, json=client).json()
