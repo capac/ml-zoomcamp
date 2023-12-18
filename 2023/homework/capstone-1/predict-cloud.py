b8b2625732cac57eaa03eca2aaee149ad77b78ed
capac <capac@users.noreply.github.com>
2023-12-18 13:54:44 +0000

    Reformatted files

--- 2023/homework/capstone-1/predict-cloud.py
+++ 2023/homework/capstone-1/predict-cloud.py
@@ -1,20 +1,46 @@
 import requests
 
 host = "zoomcamp-midterm-project.onrender.com"
-url = f'https://{host}/predict'
+url = f"https://{host}/predict"
 
 
 patient_dict = {
-    1: {"age":62.0,"anaemia":"No","creatinine_phosphokinase":231,"diabetes":"No","ejection_fraction":25,"high_blood_pressure":"Yes","platelets":253000.0,"serum_creatinine":0.9,"serum_sodium":140,"sex":"Male","smoking":"Yes","time":10},
-    2: {"age":50.0,"anaemia":"Yes","creatinine_phosphokinase":159,"diabetes":"Yes","ejection_fraction":30,"high_blood_pressure":"No","platelets":302000.0,"serum_creatinine":1.2,"serum_sodium":138,"sex":"Female","smoking":"No","time":29}
+    1: {
+        "age": 62.0,
+        "anaemia": "No",
+        "creatinine_phosphokinase": 231,
+        "diabetes": "No",
+        "ejection_fraction": 25,
+        "high_blood_pressure": "Yes",
+        "platelets": 253000.0,
+        "serum_creatinine": 0.9,
+        "serum_sodium": 140,
+        "sex": "Male",
+        "smoking": "Yes",
+        "time": 10,
+    },
+    2: {
+        "age": 50.0,
+        "anaemia": "Yes",
+        "creatinine_phosphokinase": 159,
+        "diabetes": "Yes",
+        "ejection_fraction": 30,
+        "high_blood_pressure": "No",
+        "platelets": 302000.0,
+        "serum_creatinine": 1.2,
+        "serum_sodium": 138,
+        "sex": "Female",
+        "smoking": "No",
+        "time": 29,
+    },
 }
 
 
 for key, value in patient_dict.items():
     result = requests.post(url, json=value).json()
-    print(f'Patient {key}: {patient_dict[key]}')
-    if result['outcome']:
-        print(f'''Outcome of death for patient {str(key)}: Yes.''')
+    print(f"Patient {key}: {patient_dict[key]}")
+    if result["outcome"]:
+        print(f"""Outcome of death for patient {str(key)}: Yes.""")
     else:
-        print(f'''Outcome of death for patient {str(key)}: No.''')
+        print(f"""Outcome of death for patient {str(key)}: No.""")
     print()
