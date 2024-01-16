144111929cabd366a6b94e7b3fbb9cc6b9832d06
capac <capac@users.noreply.github.com>
2024-01-16 17:57:42 +0000

    Processed images are only cropped on dog figure

--- 2023/capstone-2/data_selection.py
+++ 2023/capstone-2/data_selection.py
@@ -8,37 +8,46 @@ import xml.etree.ElementTree as ET
 from skimage.io import imread
 from skimage.transform import resize
 from PIL import Image
-from sklearn.model_selection import train_test_split
 from time import time
 
-
-images_url = 'http://vision.stanford.edu/aditya86/ImageNetDogs/images.tar'
-annotations_url = 'http://vision.stanford.edu/aditya86/ImageNetDogs/annotation.tar'
 images_dir = 'data/raw/Images'
 annotation_dir = 'data/raw/Annotation'
 processed_dir = 'data/processed/'
+img_size = 300
 
 breed_dir_list = [name for name in os.listdir(images_dir) if name not in ['.DS_Store']]
-breed_dict = {}
+
+numeric_label_dict = {}
+for index, breed_dir in enumerate(breed_dir_list):
+    numeric_label_dict[breed_dir] = index
+
+# dictionary that associates directory name with breed name
+fullname_label_dict = {}
+# dictionary with number of images of each breed in directory
+breed_num_dict = {}
 for breed_dir in breed_dir_list:
-    full_breed_dir = Path.cwd() / images_dir / breed_dir
     breed_name = re.sub(r'(n[0-9]+)-(\w+\-?\_?)', r'\2', breed_dir)
     breed_name = re.sub(r'-', r'_', breed_name.lower())
-    breed_dict[breed_name] = len(list(full_breed_dir.glob('**/*.jpg')))
+    fullname_label_dict[breed_dir] = breed_name
 
+    full_breed_dir = Path.cwd() / images_dir / breed_dir
+    breed_num_dict[breed_name] = len(list(full_breed_dir.glob('**/*.jpg')))
 
 # modified from https://www.kaggle.com/code/hengzheng/dog-breeds-classifier/notebook
 if not Path(processed_dir).exists():
     Path.mkdir(Path.cwd() / processed_dir, exist_ok=True)
     for breed_dir in breed_dir_list:
         Path.mkdir(Path.cwd() / processed_dir / breed_dir, exist_ok=True)
-    print(f'''Created {len(os.listdir(processed_dir))} folders to store cropped images of the different breeds.''')
+    print(f'Created {len(os.listdir(processed_dir))} folders to store cropped images of the different breeds.')
 else:
     print(f'Folder {processed_dir} already exists.')
 
+breed_processed_dir_list = [name for name in os.listdir(processed_dir) if name not in ['.DS_Store']]
+
 t0 = time()
+print(f'Saving {img_size}x{img_size} image sizes.')
 if not list(Path(processed_dir).glob('**/*.jpg')):
-    for breed_dir in os.listdir(processed_dir):
+    for breed_dir in breed_processed_dir_list:
         counter = 0
         for breed_file in os.listdir(f'{annotation_dir}/{breed_dir}'):
             img = Image.open(f'{images_dir}/{breed_dir}/{breed_file}.jpg')
@@ -49,7 +58,7 @@ if not list(Path(processed_dir).glob('**/*.jpg')):
             ymax = int(tree.getroot().findall('object')[0].find('bndbox').find('ymax').text)
             img = img.crop((xmin, ymin, xmax, ymax))
             img = img.convert('RGB')
-            img = img.resize((224, 224))
+            img = img.resize((img_size, img_size))
             img.save(processed_dir + breed_dir + '/' + breed_file + '.jpg')
             counter += 1
         print(f'Saved {counter} images in {breed_dir} folder.')
@@ -57,26 +66,20 @@ if not list(Path(processed_dir).glob('**/*.jpg')):
 else:
     print(f'Folder {processed_dir} with cropped images already exists.')
 
-numeric_label_maps = {}
-for num, breed_dir in enumerate(breed_dir_list):
-    numeric_label_maps[breed_dir] = num
-
-fullname_label_maps = {}
-for num, breed_dir in enumerate(breed_dir_list):
-    breed_name = re.sub(r'(n[0-9]+)-(\w+\-?\_?)', r'\2', breed_dir)
-    breed_name = re.sub(r'-', r'_', breed_name.lower())
-    fullname_label_maps[breed_dir] = breed_name
-
 
 def image_paths_and_labels():
     paths = list()
     labels = list()
     for breed_dir in breed_dir_list:
         base_dir = f'{processed_dir}{breed_dir}'
+        labels.append(fullname_label_dict[breed_dir])
         for img_file in os.listdir(base_dir):
             paths.append(f'{base_dir}/{img_file}')
-            labels.append(label_maps[breed_dir])
     return paths, labels
 
 
 paths, labels = image_paths_and_labels()
+# print(f'Paths: {paths}')
+print(f'Number of image paths: {len(paths)}')
+# print(f'Labels: {labels}')
+print(f'Number of labels: {len(labels)}')
