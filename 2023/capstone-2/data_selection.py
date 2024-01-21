ea3506265a9e4017671bbf1d96f77826b88863af
capac <capac@users.noreply.github.com>
2024-01-21 15:38:24 +0000

    Updated to pip install and load skimage

--- 2023/capstone-2/data_selection.py
+++ 2023/capstone-2/data_selection.py
@@ -5,31 +5,41 @@ import os
 import re
 from pathlib import Path
 import xml.etree.ElementTree as ET
-from skimage.io import imread
-from skimage.transform import resize
 from PIL import Image
 from time import time
 
-images_dir = 'data/raw/Images'
-annotation_dir = 'data/raw/Annotation'
-processed_dir = 'data/processed'
-img_size = 300
+try:
+    import skimage
+    print('Module skimage is installed.')
+except ImportError:
+    print('Module skimage is not installed, installing it now with pip.')
+    p1 = subprocess.run('python -m pip install scikit-image -q', shell=True,
+                        check=True, capture_output=True, text=True)
+    if p1.stderr:
+          print(p1.stderr)
+from skimage.io import imread
+from skimage.transform import resize
+
+images_dir = Path.cwd() / 'data/raw/Images'
+annotation_dir = Path.cwd() / 'data/raw/Annotation'
+processed_dir = Path.cwd() / 'data/processed'
+img_size = 299
 
 # modified from https://www.kaggle.com/code/hengzheng/dog-breeds-classifier/notebook
 breed_dir_list = [name for name in os.listdir(images_dir) if name not in ['.DS_Store']]
-if not Path(processed_dir).exists():
-    Path.mkdir(Path.cwd() / processed_dir, exist_ok=True)
+if not processed_dir.exists():
+    Path.mkdir(processed_dir, exist_ok=True)
     for breed_dir in breed_dir_list:
         breed_name = re.sub(r'(n[0-9]+)-(\w+\-?\_?)', r'\2', breed_dir)
         breed_name = re.sub(r'-', r'_', breed_name.lower())
-        Path.mkdir(Path.cwd() / processed_dir / breed_name, exist_ok=True)
+        Path.mkdir(processed_dir / breed_name, exist_ok=True)
     print(f'''Created {len(os.listdir(processed_dir))} folders to store cropped images of the different breeds.''')
 else:
     print(f'Folder {processed_dir} already exists.')
 
 t0 = time()
 print(f'Saving {img_size}x{img_size} image sizes.')
-if not list(Path(processed_dir).glob('**/*.jpg')):
+if not list(processed_dir.glob('**/*.jpg')):
     for breed_dir in breed_dir_list:
         counter = 0
         for breed_prefix in os.listdir(f'{annotation_dir}/{breed_dir}'):
@@ -44,7 +54,7 @@ if not list(Path(processed_dir).glob('**/*.jpg')):
             img = img.resize((img_size, img_size))
             breed_name = re.sub(r'(n[0-9]+)-(\w+\-?\_?)', r'\2', breed_dir)
             breed_name = re.sub(r'-', r'_', breed_name.lower())
-            img.save(processed_dir + '/' + breed_name + '/' + breed_prefix + '.jpg')
+            img.save(str(processed_dir) + '/' + breed_name + '/' + breed_prefix + '.jpg')
             counter += 1
         print(f'Saved {counter} images in {breed_name} folder.')
     print(f'Time elapsed: {round(time()-t0, 0)} seconds.')
