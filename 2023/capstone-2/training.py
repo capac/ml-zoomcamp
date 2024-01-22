b89d8695406062bc3e6f8a52348de480c2bebb2c
capac <541813+capac@users.noreply.github.com>
2024-01-22 13:44:04 +0000

    Added standalone training script

---
+++ 2023/capstone-2/training.py
@@ -0,0 +1,295 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import re
+import subprocess
+from pathlib import Path
+from time import time
+import xml.etree.ElementTree as ET
+from PIL import Image
+import numpy as np
+
+try:
+    import skimage
+    print('Module skimage is installed.')
+except ImportError:
+    print('Module skimage is not installed, installing it now with pip.')
+    p1 = subprocess.run('python -m pip install scikit-image -q', shell=True,
+                        check=True, capture_output=True, text=True)
+    if p1.stderr:
+          print(p1.stderr)
+from skimage.io import imread
+from skimage.transform import resize
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.applications.xception import Xception, preprocess_input, decode_predictions
+from tensorflow.keras.preprocessing.image import load_img
+
+images_dir = Path.cwd() / 'data/raw/Images'
+annotation_dir = Path.cwd() / 'data/raw/Annotation'
+processed_dir = Path.cwd() / 'data/processed'
+
+
+# Data preparation
+
+# folder names come from namimg in raw/Images and raw/Annotation folders
+breed_dir_list = ['n02085936-Maltese_dog', 'n02088094-Afghan_hound', 'n02092002-Scottish_deerhound',
+                  'n02112018-Pomeranian', 'n02107683-Bernese_mountain_dog', 'n02111889-Samoyed',
+                  'n02090721-Irish_wolfhound', 'n02086240-Shih-Tzu', 'n02111500-Great_Pyrenees',
+                  'n02111129-Leonberg']
+
+# modified from https://www.kaggle.com/code/hengzheng/dog-breeds-classifier/notebook
+if not processed_dir.exists():
+    Path.mkdir(processed_dir, exist_ok=True)
+    for breed_dir in breed_dir_list:
+        breed_name = re.sub(r'(n[0-9]+)-(\w+\-?\_?)', r'\2', breed_dir)
+        breed_name = re.sub(r'-', r'_', breed_name.lower())
+        Path.mkdir(processed_dir / breed_name, exist_ok=True)
+    print(f'''Created {len(os.listdir(processed_dir))} folders to store cropped images of the different breeds.''')
+else:
+    print(f'Folder {processed_dir} already exists.')
+
+t0 = time()
+img_size = 299
+print(f'Saving {img_size}x{img_size} image sizes.')
+if not list(processed_dir.glob('**/*.jpg')):
+    for breed_dir in breed_dir_list:
+        counter = 0
+        for breed_prefix in os.listdir(f'{annotation_dir}/{breed_dir}'):
+            img = Image.open(f'{images_dir}/{breed_dir}/{breed_prefix}.jpg')
+            tree = ET.parse(f'{annotation_dir}/{breed_dir}/{breed_prefix}')
+            xmin = int(tree.getroot().findall('object')[0].find('bndbox').find('xmin').text)
+            xmax = int(tree.getroot().findall('object')[0].find('bndbox').find('xmax').text)
+            ymin = int(tree.getroot().findall('object')[0].find('bndbox').find('ymin').text)
+            ymax = int(tree.getroot().findall('object')[0].find('bndbox').find('ymax').text)
+            img = img.crop((xmin, ymin, xmax, ymax))
+            img = img.convert('RGB')
+            img = img.resize((img_size, img_size))
+            breed_name = re.sub(r'(n[0-9]+)-(\w+\-?\_?)', r'\2', breed_dir)
+            breed_name = re.sub(r'-', r'_', breed_name.lower())
+            img.save(str(processed_dir) + '/' + breed_name + '/' + breed_prefix + '.jpg')
+            counter += 1
+        print(f'Saved {counter} images in {breed_name} folder.')
+    print(f'Time elapsed: {round(time()-t0, 0)} seconds.')
+else:
+    print(f'Folder {str(processed_dir)} with cropped images already exists.')
+
+
+# Removal of corrupted images
+num_skipped = 0
+for folder_name in breed_dir_list:
+    folder_name = re.sub(r'(n[0-9]+)-(\w+\-?\_?)', r'\2', str(folder_name))
+    folder_name = re.sub(r'-', r'_', folder_name.lower())
+    breed_dir = processed_dir / folder_name
+    print(f'Looking into folder {str(breed_dir)}')
+    for fname in os.listdir(breed_dir):
+        fpath = breed_dir / fname
+        try:
+            fobj = open(fpath, "rb")
+            is_jfif = b"JFIF" in fobj.peek(10)
+        finally:
+            fobj.close()
+        if not is_jfif:
+            num_skipped += 1
+            # Delete corrupted image
+            os.remove(fpath)
+print(f"Deleted {num_skipped} images.")
+
+
+# Data generation
+new_img_size = 150
+image_size_tuple = (new_img_size, new_img_size)
+batch_size = 32
+
+train_ds, val_ds = keras.utils.image_dataset_from_directory(
+    processed_dir,
+    validation_split = 0.3,
+    subset = 'both',
+    seed = 33,
+    labels = 'inferred',
+    label_mode = 'categorical',
+    image_size = image_size_tuple,
+    batch_size = batch_size,
+    shuffle=True,
+)
+
+val_batches = tf.data.experimental.cardinality(val_ds)
+test_ds = val_ds.take((1*val_batches) // 2)
+val_ds = val_ds.skip((1*val_batches) // 2)
+
+names = train_ds.class_names
+
+# Transfer learning
+
+# Using a small image size to decrease training time.
+base_model = Xception(weights='imagenet', include_top=False, input_shape=(new_img_size, new_img_size, 3))
+base_model.trainable = False
+inputs = keras.Input(shape=(new_img_size, new_img_size, 3))
+base = base_model(inputs, training=False)
+vectors = keras.layers.GlobalAveragePooling2D()(base)
+outputs = keras.layers.Dense(10)(vectors)
+model = keras.Model(inputs, outputs)
+
+learning_rate = 0.01
+optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+loss = keras.losses.CategoricalCrossentropy(from_logits=True)
+model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
+
+history = model.fit(train_ds, epochs = 10, validation_data = val_ds)
+
+# Data augmentation
+
+data_augmentation_layers = [
+    keras.layers.RandomFlip("horizontal"),
+    keras.layers.RandomRotation(0.1),
+]
+
+def data_augmentation(images):
+    for layer in data_augmentation_layers:
+        images = layer(images)
+    return images
+
+base_model = Xception(weights='imagenet', include_top=False, input_shape=(new_img_size, new_img_size, 3))
+base_model.trainable = False
+inputs = keras.Input(shape=(new_img_size, new_img_size, 3))
+augmented_inputs = data_augmentation(inputs)
+scaled_inputs = keras.layers.Rescaling(1./255)(augmented_inputs)
+base = base_model(scaled_inputs, training=False)
+vectors = keras.layers.GlobalAveragePooling2D()(base)
+outputs = keras.layers.Dense(10)(vectors)
+model = keras.Model(inputs, outputs)
+
+learning_rate = 0.01
+optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+loss = keras.losses.CategoricalCrossentropy(from_logits=True)
+model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
+
+history = model.fit(train_ds, epochs = 10, validation_data = val_ds)
+
+## Tuning the learning rate
+
+def make_model(learning_rate=0.01):
+    base_model = Xception(weights='imagenet', include_top=False, input_shape=(new_img_size, new_img_size, 3))
+    base_model.trainable = False
+    inputs = keras.Input(shape=(new_img_size, new_img_size, 3))
+    augmented_inputs = data_augmentation(inputs)
+    scaled_inputs = keras.layers.Rescaling(1./255)(augmented_inputs)
+    base = base_model(scaled_inputs, training=False)
+    vectors = keras.layers.GlobalAveragePooling2D()(base)
+    outputs = keras.layers.Dense(10)(vectors)
+    model = keras.Model(inputs, outputs)
+    optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+    loss = keras.losses.CategoricalCrossentropy(from_logits=True)
+    model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])    
+    return model
+
+learning_rate_scores = {}
+for lr in [0.0001, 0.001, 0.01, 0.1]:
+    print(f'Learning rate: {lr}')
+    model = make_model(learning_rate=lr)
+    history = model.fit(train_ds, epochs=10, validation_data=val_ds)
+    learning_rate_scores[lr] = history.history
+    print()
+    print('*****'*10)
+    print()
+
+# Adjusting inner layers
+
+def make_model(learning_rate=0.01, size_inner=100):
+    base_model = Xception(weights='imagenet', include_top=False, input_shape=(new_img_size, new_img_size, 3))
+    base_model.trainable = False
+    inputs = keras.Input(shape=(new_img_size, new_img_size, 3))
+    augmented_inputs = data_augmentation(inputs)
+    scaled_inputs = keras.layers.Rescaling(1./255)(augmented_inputs)
+    base = base_model(scaled_inputs, training=False)
+    vectors = keras.layers.GlobalAveragePooling2D()(base)
+    inner = keras.layers.Dense(size_inner, activation='relu')(vectors)
+    outputs = keras.layers.Dense(10)(inner)
+    model = keras.Model(inputs, outputs)
+    optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+    loss = keras.losses.CategoricalCrossentropy(from_logits=True)
+    model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])    
+    return model
+
+
+best_learning_rate = 0.001
+size_scores = {}
+for size in [10, 100, 1000]:
+    print(f'Inner layer size: {size}')
+    model = make_model(learning_rate=best_learning_rate, size_inner=size)
+    history = model.fit(train_ds, epochs=10, validation_data=val_ds)
+    size_scores[size] = history.history
+    print()
+    print('*****'*10)
+    print()
+
+# Adding dropout
+
+def make_model(learning_rate=0.01, size_inner=100, droprate=0.5):
+    base_model = Xception(weights='imagenet', include_top=False, input_shape=(new_img_size, new_img_size, 3))
+    base_model.trainable = False
+    inputs = keras.Input(shape=(new_img_size, new_img_size, 3))
+    augmented_inputs = data_augmentation(inputs)
+    scaled_inputs = keras.layers.Rescaling(1./255)(augmented_inputs)
+    base = base_model(scaled_inputs, training=False)
+    vectors = keras.layers.GlobalAveragePooling2D()(base)
+    inner = keras.layers.Dense(size_inner, activation='relu')(vectors)
+    drop = keras.layers.Dropout(droprate)(inner)
+    outputs = keras.layers.Dense(10)(drop)
+    model = keras.Model(inputs, outputs)
+    optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+    loss = keras.losses.CategoricalCrossentropy(from_logits=True)
+    model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])    
+    return model
+
+best_learning_rate = 0.001
+best_size = 1000
+droprate_scores = {}
+for droprate in [0.0, 0.2, 0.5, 0.8]:
+    print(f'Dropout rate: {droprate}')
+    model = make_model(learning_rate=best_learning_rate, size_inner=best_size, droprate=droprate)
+    history = model.fit(train_ds, epochs=30, validation_data=val_ds)
+    droprate_scores[droprate] = history.history
+    print()
+    print('*****'*10)
+    print()
+
+
+# Saving best model with checkpointing
+
+checkpoint = keras.callbacks.ModelCheckpoint(
+    'xception_v1_{epoch:02d}_{val_accuracy:.3f}.h5',
+    save_best_only=True,
+    monitor='val_accuracy',
+    mode='max'
+)
+
+# best hyperparameter values
+learning_rate = 0.001
+size = 1000
+droprate = 0.8
+
+model = make_model(learning_rate=learning_rate, size_inner=size, droprate=droprate)
+
+history = model.fit(train_ds, epochs=50, validation_data=val_ds, callbacks=[checkpoint])
+
+# Testing the model
+
+best_model = keras.models.load_model('xception_v1_48_0.968.h5')
+
+loss, test_acc = best_model.evaluate(test_ds)
+print(f'Test accuracy: {test_acc:.4f}')
+
+
+path_to_image = '/home/jovyan/workspace/ml-zoomcamp/2023/capstone-2/data/test/samoyed.jpg'
+img = keras.utils.load_img(path_to_image, target_size=image_size_tuple)
+img_array = keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+predictions = best_model.predict(img_array)
+
+# print the prediction results
+print(dict(zip(names, predictions[0])))
