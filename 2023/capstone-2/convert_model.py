e78e3c8b796a16de2038a66b345538f6c6b9a3b7
capac <capac@users.noreply.github.com>
2024-01-23 14:50:22 +0000

    Converts HDF5 model file to TFLite file

---
+++ 2023/capstone-2/convert_model.py
@@ -0,0 +1,16 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow as tf
+from tensorflow import keras
+
+original_model_filename = 'xception_v1_48_0.968.h5'
+model = keras.models.load_model(original_model_filename, compile=False)
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+tflite_filename = 'top_10_dog_breeds.tflite'
+with open(tflite_filename, 'wb') as f_out:
+    f_out.write(tflite_model)
+
+print(f'Saved {original_model_filename} to {tflite_filename}.')
