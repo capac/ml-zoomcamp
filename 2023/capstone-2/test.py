3442e790ec0654ca1c4f9668c86326aecc535753
capac <capac@users.noreply.github.com>
2024-01-24 13:39:45 +0000

    Using pretty print for output

--- 2023/capstone-2/test.py
+++ 2023/capstone-2/test.py
@@ -1,4 +1,5 @@
 import requests
+from pprint import pprint
 
 url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
 UNSPLASH_URL = 'https://images.unsplash.com/photo-1636496430627-a7b203b4cc58?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
@@ -6,4 +7,4 @@ UNSPLASH_URL = 'https://images.unsplash.com/photo-1636496430627-a7b203b4cc58?q=8
 data = {'url': UNSPLASH_URL}
 
 result = requests.post(url, json=data).json()
-print(result)
+pprint(result)
