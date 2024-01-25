4a7ed2862ede007b9755165eebe0ae2f81b307bb
capac <capac@users.noreply.github.com>
2024-01-25 19:40:41 +0000

    Added AWS link and using pretty print for output

--- 2023/capstone-2/test_cloud.py
+++ 2023/capstone-2/test_cloud.py
@@ -1,11 +1,12 @@
 import requests
+from pprint import pprint
 
-host = "capstone-project-2-kpsf.onrender.com"
+host = "rv4m3mgwb5.execute-api.eu-west-1.amazonaws.com/test"
 url = f"https://{host}/predict"
 
-UNSPLASH_URL = 'https://images.unsplash.com/photo-1636496430627-a7b203b4cc58?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+UNSPLASH_URL = "https://images.unsplash.com/photo-1636496430627-a7b203b4cc58"
 
 data = {'url': UNSPLASH_URL}
 
 result = requests.post(url, json=data).json()
-print(result)
+pprint(result)
