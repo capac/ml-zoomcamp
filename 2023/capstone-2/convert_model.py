24e945915603e9f354ceaac80ffdc60589ef6a7e
capac <541813+capac@users.noreply.github.com>
2024-03-02 15:49:32 +0000

    Changed file name for 120 breed classifier

--- 2023/capstone-2/convert_model.py
+++ 2023/capstone-2/convert_model.py
@@ -4,12 +4,12 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 from tensorflow import keras
 
-original_model_filename = 'models_30_breeds/xception_v1_14_0.942.h5'
+original_model_filename = 'xception_v1_21_0.797.h5'
 model = keras.models.load_model(original_model_filename, compile=False)
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
 
-tflite_filename = 'models_30_breeds/top_30_dog_breeds.tflite'
+tflite_filename = 'top_120_dog_breeds.tflite'
 with open(tflite_filename, 'wb') as f_out:
     f_out.write(tflite_model)
 
