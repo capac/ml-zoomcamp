98ba99ccc06c5a6c225af54fca555f67b134ba9f
capac <541813+capac@users.noreply.github.com>
2024-03-02 12:09:43 +0000

    Updated input directory

--- 2023/capstone-2/convert_model.py
+++ 2023/capstone-2/convert_model.py
@@ -4,7 +4,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 from tensorflow import keras
 
-original_model_filename = 'xception_v1_14_0.942.h5'
+original_model_filename = 'models_30_breeds/xception_v1_14_0.942.h5'
 model = keras.models.load_model(original_model_filename, compile=False)
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
