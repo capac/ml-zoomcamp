8711fb9e3b775267c385b2facad186151a2e9aa3
capac <capac@users.noreply.github.com>
2024-05-14 14:46:04 +0100

    Commeneted out code to install skimage package, since it's only needed on Saturn Cloud instance

--- 2023/capstone-2/data_selection.py
+++ 2023/capstone-2/data_selection.py
@@ -2,40 +2,43 @@
 # coding: utf-8
 
 import os
+# import subprocess
 import re
 from pathlib import Path
 import xml.etree.ElementTree as ET
 from PIL import Image
 from time import time
 
-try:
-    import skimage
-    print('Module skimage is installed.')
-except ImportError:
-    print('Module skimage is not installed, installing it now with pip.')
-    p1 = subprocess.run('python -m pip install scikit-image -q', shell=True,
-                        check=True, capture_output=True, text=True)
-    if p1.stderr:
-          print(p1.stderr)
-    else:
-        print('Installed scikit-image with pip.')
-from skimage.io import imread
-from skimage.transform import resize
+# try:
+#     import skimage
+#     print('Module skimage is installed.')
+# except ImportError:
+#     print('Module skimage is not installed, installing it now with pip.')
+#     p1 = subprocess.run('python -m pip install scikit-image -q', shell=True,
+#                         check=True, capture_output=True, text=True)
+#     if p1.stderr:
+#         print(p1.stderr)
+#     else:
+#         print('Installed scikit-image with pip.')
+# from skimage.io import imread
+# from skimage.transform import resize
 
 images_dir = Path.cwd() / 'data/raw/Images'
 annotation_dir = Path.cwd() / 'data/raw/Annotation'
 processed_dir = Path.cwd() / 'data/processed'
 img_size = 299
 
-# modified from https://www.kaggle.com/code/hengzheng/dog-breeds-classifier/notebook
-breed_dir_list = [name for name in os.listdir(images_dir) if name not in ['.DS_Store']]
+# from https://www.kaggle.com/code/hengzheng/dog-breeds-classifier/notebook
+breed_dir_list = [name for name in os.listdir(images_dir)
+                  if name not in ['.DS_Store']]
 if not processed_dir.exists():
     Path.mkdir(processed_dir, exist_ok=True)
     for breed_dir in breed_dir_list:
         breed_name = re.sub(r'(n[0-9]+)-(\w+\-?\_?)', r'\2', breed_dir)
         breed_name = re.sub(r'-', r'_', breed_name.lower())
         Path.mkdir(processed_dir / breed_name, exist_ok=True)
-    print(f'''Created {len(os.listdir(processed_dir))} folders to store cropped images of the different breeds.''')
+    print(f'''Created {len(os.listdir(processed_dir))} folders to store '''
+          f'''cropped images of the different breeds.''')
 else:
     print(f'Folder {str(processed_dir)} already exists.')
 
@@ -47,16 +50,21 @@ if not list(processed_dir.glob('**/*.jpg')):
         for breed_prefix in os.listdir(f'{annotation_dir}/{breed_dir}'):
             img = Image.open(f'{images_dir}/{breed_dir}/{breed_prefix}.jpg')
             tree = ET.parse(f'{annotation_dir}/{breed_dir}/{breed_prefix}')
-            xmin = int(tree.getroot().findall('object')[0].find('bndbox').find('xmin').text)
-            xmax = int(tree.getroot().findall('object')[0].find('bndbox').find('xmax').text)
-            ymin = int(tree.getroot().findall('object')[0].find('bndbox').find('ymin').text)
-            ymax = int(tree.getroot().findall('object')[0].find('bndbox').find('ymax').text)
+            xmin = int(tree.getroot().findall('object')[0].find('bndbox').
+                       find('xmin').text)
+            xmax = int(tree.getroot().findall('object')[0].find('bndbox').
+                       find('xmax').text)
+            ymin = int(tree.getroot().findall('object')[0].find('bndbox').
+                       find('ymin').text)
+            ymax = int(tree.getroot().findall('object')[0].find('bndbox').
+                       find('ymax').text)
             img = img.crop((xmin, ymin, xmax, ymax))
             img = img.convert('RGB')
             img = img.resize((img_size, img_size))
             breed_name = re.sub(r'(n[0-9]+)-(\w+\-?\_?)', r'\2', breed_dir)
             breed_name = re.sub(r'-', r'_', breed_name.lower())
-            img.save(str(processed_dir) + '/' + breed_name + '/' + breed_prefix + '.jpg')
+            img.save(str(processed_dir) + '/' + breed_name + '/' +
+                     breed_prefix + '.jpg')
             counter += 1
         print(f'Saved {counter} images in {breed_name} folder.')
     print(f'Time elapsed: {round(time() - t0, 0)} seconds.')
